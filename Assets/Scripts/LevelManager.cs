18e332ba6cbe43ab7a5a5bb6dd1a7d80db2bc5b2
Daniel <danielmilo12@gmail.com>
2025-01-26 10:46:31 -0800

    1-4 works

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,11 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
     public BarksManager barksManager;
+
+    public int CurrentLevel;
+
+    public TextMeshProUGUI textMeshProUGUI;
+
+    float startTime;
+
+    private void Awake()
+    {
+        startTime = Time.time;
+    }
 
     public void loadLevel(int level)
     {
@@ -22,6 +35,51 @@ public class LevelManager : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.R)) 
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        if (Time.time - startTime > 3)
+        {
+            CheckForWin();
+        }
+    }
+
+    private void CheckForWin()
+    {
+        switch (CurrentLevel)
+        {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+                {
+                    if(Actor.actors.Count == 1)
+                    {
+                        // win
+                        _ = OnWin();
+                    }
+                    break;
+                }
+            case 5:
+                {
+                    break;
+                }
+            case 6:
+                {
+                    break;
+                }
+        }
+    }
+
+    bool won = false;
+    private async Task OnWin()
+    {
+        if (!won)
+        {
+            won = true;
+            textMeshProUGUI.gameObject.SetActive(true);
+            await Task.Delay(2000);
+
+            SceneManager.LoadScene(CurrentLevel + 1);
         }
     }
 }
